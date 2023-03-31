1dd23cbd11745d40d85b197a260f130d695b6f3e
lookeke <xiconz@qq.com>
2023-03-31 12:58:12 +0800

    test: 测试gitlab ci runner

---
+++ client/cmd/download.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
+apk update
+apk add which sshpass rsync openssh-client bash curl tar
+
+which sshpass
+which rsync
+which sshpass
+which ssh
+which scp
+which sh
+which bash
